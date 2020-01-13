b0d8a869138f164a8fc066397f7bd55b63382191
Paris Cornett <pariscornett@gmail.com>
2020-01-12 19:04:31 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+
+// Namespace (keeps things organized)
+namespace GuessThatNumber
+{
+    //Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            //A few C# notes before getting started
+            //string name = "Paris Cornett";
+            //int age = 26;
+
+            //Console.WriteLine(name + " is " + age + " years old 👑" );
+            //Line 16 works fine, but the better way to write this would be as follows:
+            //Console.WriteLine("{0} is {1} years old 👩🏻‍💻", name, age);
+            //if you wanted to add more variables, you would add more numbered brackets (in ascending order) and more variable names outside of quotations.
+
+            //STARTING POINT
+        }
+    }
+}
